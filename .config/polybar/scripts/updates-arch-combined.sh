0daeb80bcea9f47bbb140706ff4902bfc2c589c1
MetalNeox <codematrix@altervista.org>
2019-03-25 21:36:10 +0100

    Update polybar colors

--- .config/polybar/scripts/updates-arch-combined.sh
+++ .config/polybar/scripts/updates-arch-combined.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+#echo "%{F#66818b}${daemon}%{F-}"
+
 if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
     updates_arch=0
 fi
@@ -13,12 +15,14 @@ fi
 #updates=$(("$updates_arch" + "$updates_aur"))
 
 if [ "$updates_arch" -gt 0 ]; then
-    echo -n "$updates_arch  " 
+    #echo -n "$updates_arch  " 
+    echo -n "$updates_arch %{F#FF0000}%{F-} "
 else
-    echo -n "0  "
+    echo -n "0 %{F#00FF00}%{F-} "
 fi
 if [ "$updates_aur" -gt 0 ]; then
-    echo -n "$updates_aur"
+#    echo -n "$updates_aur"
+    echo -n "%{F#FFA500}$updates_aur%{F-}"
 else
-    echo -n ""
+    echo -n "0"
 fi
