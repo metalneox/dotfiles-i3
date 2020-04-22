e4282d5a9942bab3521d41700ff9d782ea83e19b
MetalNeox <codematrix@altervista.org>
2020-04-22 16:05:20 +0200

    add mini-video

--- .config/i3/scripts/video.sh
+++ .config/i3/scripts/video.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DIALOG_RESULT=$(echo -e 'Normal Video\nVideo no Audio\nMini Video' | rofi -dmenu -i -p "[SYSTEM]" -hide-scrollbar -tokenize -lines 6 -eh 1 -width 25 -location 0 -xoffset 0 -yoffset 0 -padding 20 -disable-history -font "ClearSansMedium 14")
+DIALOG_RESULT=$(echo -e 'Normal Video\nAudio no Video\nMini Video' | rofi -dmenu -i -p "[SYSTEM]" -hide-scrollbar -tokenize -lines 6 -eh 1 -width 25 -location 0 -xoffset 0 -yoffset 0 -padding 20 -disable-history -font "ClearSansMedium 14")
 
 echo "This result is : $DIALOG_RESULT"
 sleep 1;
@@ -9,11 +9,13 @@ if [ "$DIALOG_RESULT" = "Normal Video" ];
 then
     notify-send -t 3000 --icon=video-display "Playing Video" "$(xclip -o)";
     mpv --ytdl-format=22 --fs "$(xclip -o)" || mpv --fs "$(xclip -o)";
-elif [ "$DIALOG_RESULT" = "Video no Audio" ];
+elif [ "$DIALOG_RESULT" = "Audio no Video" ];
 then
     notify-send -t 3000 --icon=video-display "Playing Video" "$(xclip -o)";
     mpv --no-video --ytdl-format=22 --fs "$(xclip -o)" || mpv --fs "$(xclip -o)";
 elif [ "$DIALOG_RESULT" = "Mini Video" ];
 then
-    notify-send -t 3000 --icon=video-display "Non implementato";
+    notify-send -t 3000 --icon=video-display "Playing Mini Video" "$(xclip -o)";
+    mpv --ytdl-format=22 --geometry=20%x20%+100%+100% "$(xclip -o)" || mpv --geometry=20%x20%+100%+100% "$(xclip -o)";
+    #notify-send -t 3000 --icon=video-display "Non implementato";
 fi
