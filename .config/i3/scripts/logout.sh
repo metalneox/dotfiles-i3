4e284d92a0db76ff4a571037e3945652753b2d2d
MetalNeox <metalneox@gmail.com>
2021-05-27 10:54:25 +0200

    Massive new configuration

--- .config/i3/scripts/logout.sh
+++ .config/i3/scripts/logout.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 
-DIALOG_RESULT=$(echo -e 'lock\nreboot\npoweroff\nlogout' | rofi -dmenu -i -p "[SYSTEM]" -hide-scrollbar -tokenize -lines 6 -eh 1 -width 25 -location 0 -xoffset 0 -yoffset 0 -padding 20 -disable-history -font "ClearSansMedium 14")
+DIALOG_RESULT=$(echo -e 'Task Manager\nLock\nReboot\nPoweroff\nLogout' | rofi -dmenu -i -p "[SYSTEM]" -hide-scrollbar -tokenize -lines 6 -eh 1 -width 25 -location 0 -xoffset 0 -yoffset 0 -padding 20 -disable-history -font "ClearSansMedium 14")
 
 echo "This result is : $DIALOG_RESULT"
 sleep 1;
 
-if [ "$DIALOG_RESULT" = "lock" ];
+if [ "$DIALOG_RESULT" = "Task Manager" ];
+then
+    exec htop
+elif [ "$DIALOG_RESULT" = "Lock" ];
 then
     exec betterlockscreen -l dim
-elif [ "$DIALOG_RESULT" = "reboot" ];
+elif [ "$DIALOG_RESULT" = "Reboot" ];
 then
     exec systemctl reboot
-elif [ "$DIALOG_RESULT" = "poweroff" ];
+elif [ "$DIALOG_RESULT" = "Poweroff" ];
 then
     exec systemctl poweroff
-elif [ "$DIALOG_RESULT" = "logout" ];
+elif [ "$DIALOG_RESULT" = "Logout" ];
 then
     i3-msg 'exit'
 fi
--- .profile
+++ .profile
@@ -1,2 +1,8 @@
 #mpd ~/.config/mpd/mpd.conf
+#Static random wallpaper
+feh --randomize --bg-fill ~/Background/*
+
+#Dynamic random wallpaper
+#watch n- 1200 feh --randomize --bg-fill ~/Background/*
+
 
