8eafaa57a0d841541cd18ef43318eaf253331ca2
MetalNeox <metalneox@gmail.com>
2022-12-11 23:23:20 +0100

    First conf neovim

---
+++ .config/nvim/lua/core/plugin_config/colorscheme.lua
@@ -0,0 +1,9 @@
+vim.o.termguicolors = true
+vim.o.background = 'dark'
+vim.cmd [[ colorscheme vscode ]]
+
+--vim.cmd [[ colorscheme catppuccin ]]
+--vim.cmd [[ colorscheme onedarker ]]
+--vim.cmd [[ colorscheme onenord ]]
+
+--vim.g.Guifont="DejaVu Sans Mono for Powerline:h25"
---
+++ .config/nvim/lua/core/plugins.lua
@@ -0,0 +1,67 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  -- Packer
+  use 'wbthomason/packer.nvim'
+
+  -- Theme
+  -- use { "catppuccin/nvim", as = "catppuccin" }
+  -- use "lunarvim/Onedarker.nvim"
+  --use 'rmehri01/onenord.nvim'
+  use 'Mofiqul/vscode.nvim'
+
+  -- Todo comments
+  use 'folke/todo-comments.nvim'
+
+  -- Buffer navigation
+  use 'nvim-lualine/lualine.nvim'
+
+  --Markdown Preview
+  use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install",
+        setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" },
+  })
+
+
+  -- Da valutare
+  use 'nvim-tree/nvim-tree.lua'
+  use 'nvim-tree/nvim-web-devicons'
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'lewis6991/gitsigns.nvim'
+
+  use {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+  }
+ 
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'L3MON4D3/LuaSnip' 
+
+
+
+  use {
+	  'nvim-telescope/telescope.nvim',
+	  tag = '0.1.0',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
