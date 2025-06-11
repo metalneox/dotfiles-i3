226ef7a89565b10f55892391f52f6cc891ed8eba
MetalNeox <metalneox@gmail.com>
2025-06-11 16:22:49 +0200

    Update installare

--- install.sh
+++ install.sh
@@ -7,22 +7,16 @@
 # xorg full??
 
 # Base
-sudo pacman -S git dunst nitrogen alacritty rofi rofi-calc rofi-emoji maim i3-gaps curl feh mpv firefox ttf-dejavu otf-font-awesome ttf-font-awesome ttf-ubuntu-font-family ttf-hack papirus-icon-theme picom zip rustup unrar unzip udiskie telegram-desktop tor xdg-user-dirs zsh zsh-autosuggestions zsh-syntax-highlighting zsh-theme-powerlevel10k zathura zathura-djvu zathura-pdf-mupdf python otf-font-awesome ttf-font-awesome clipmenu starship --noconfirm
-
+sudo pacman -S --needed --noconfirm - < packages
 # Plugin o altro
 sudo pacman -S python-pip --noconfirm
 
 ## Install 3rd package
-yay -S github-cli polybar visual-studio-code-bin nerd-fonts-hack ttf-weather-icons i3lock-color brave-bin betterlockscreen --noconfirm
-#
+yay -S --needed --noconfirm - < packages.aur
 
-## 
-## My current driver nvidia
-##yay -S nvidia-390xx-dkms nvidia-390xx-utils --noconfirm
-#
 ## Configuration
 #xdg-user-dirs-update
-git clone https://github.com/metalneox/dotfiles.git
+git clone https://github.com/metalneox/dotfiles-i3.git
 #
 ##Clear and install dotfiles
 rm -rf ~/.*
@@ -31,5 +25,5 @@ rm -rf dotfiles
 rm ~/install.sh
 
 #Polybar gmail
-pip install google-api-python-client oauth2client
-python ~/.config/polybar/scripts/gmail/auth.py
+#pip install google-api-python-client oauth2client
+#python ~/.config/polybar/scripts/gmail/auth.py
